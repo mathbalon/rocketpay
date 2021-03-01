7c55fa219da49ef7d1d37d8e883791293d443df8
Matheus Balonecker <mathbalon@gmail.com>
2021-02-28 22:29:45 -0400

    Teste account_controller

---
+++ test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -0,0 +1,48 @@
+defmodule RocketpayWeb.AccountsControllerTest do
+  use RocketpayWeb.ConnCase
+
+  alias Rocketpay.{Account, User}
+
+  describe "call/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "Allana",
+        password: "cleopatra",
+        nickname: "ovelha",
+        email: "allana@gmail.com",
+        age: 26
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = Rocketpay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic YmFsbzoxMjM0NTY=")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when all params are valid, make the deposit", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "50.0"}
+
+      response = conn
+      |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+      |> json_response(:ok)
+
+      assert %{
+        "account" => %{"balance" => "50.00", "id" => _id},
+        "message" => "Balance changed succefully"
+      } = response
+    end
+
+    test "when there ara invalid params, returns an error", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "invalid_error"}
+
+      response = conn
+      |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+      |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid deposit value"}
+
+      assert response == expected_response
+    end
+  end
+end
