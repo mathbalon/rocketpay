61fd291f9c686a017b14b8d3dcd97684e51d95f2
Matheus Balonecker <mathbalon@gmail.com>
2021-02-28 20:47:46 -0400

    logica de transação

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,5 +1,6 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.{Account}
+  alias Rocketpay.Accounts.Transaction.Response, as: TransactionResponse
 
   def render("update.json", %{
         account: %Account {
@@ -12,6 +13,27 @@ defmodule RocketpayWeb.AccountsView do
       account: %{
         id: id,
         balance: balance
+      }
+    }
+  end
+
+  def render("transaction.json", %{
+    transaction: %TransactionResponse{
+      from_account: from_account,
+      to_account: to_account
+      }
+    }) do
+    %{
+      message: "Succefully transaction",
+      transaction: %{
+        from: %{
+          id: from_account.id,
+          balance: from_account.balance,
+        },
+        to: %{
+          id: to_account.id,
+          balance: to_account.balance,
+        }
       }
     }
   end
